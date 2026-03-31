8b6db652cc0185b4667aeea8e333cc6b85167508
Bhavan Kumar Velladurai <hi34tihe@stud.informatik.uni-erlangen.de>
2026-03-31 20:09:19 +0200

    dummy data generation fp32 script

--- python/generate_data.py
+++ python/generate_data.py
@@ -54,14 +54,14 @@ if __name__ == "__main__":
     parser.add_argument(
         "--output_dir",
         type=str,
-        default="./data64",
+        default="./data32",
         help="Parent directory for images/ and kernels/"
     )
     parser.add_argument(
         "--dtype",
         type=str,
         choices=["float32", "float64"],
-        default="float64"
+        default="float32"
     )
     parser.add_argument(
         "--seed",
